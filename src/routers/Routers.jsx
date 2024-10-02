5fb778e3f306a214e29b593c4de5fbb6ffca0cc2
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-10-03 01:48:14 +0600

    added error page

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -3,11 +3,13 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import ErrorPages from "../pages/Error/ErrorPages";
 
 
   const router = createBrowserRouter([
     {
       path: "/",
+      errorElement: <ErrorPages></ErrorPages>,
       element: <Main></Main>,
       children: [
         {
